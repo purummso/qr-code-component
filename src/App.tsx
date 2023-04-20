0160d3e5cf9b3cb8b17ad2cf8ec93a7ce0f78673
purummso <dev@abdar.me>
2023-04-20 09:42:44 -0400

    Added attribution, challange completed, I think

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import qr_image from './assets/image-qr-code.png'
 function App() {
 
   return (
-    <div className={styles.container}>
-      <img className={styles.qr} src={qr_image} alt="QR Code that you can scan with your mobile to access example.com" />
-      <h1>Improve your front-end skills by building projects</h1>
-      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-    </div>
+    <>
+      <div className={styles.container}>
+        <img className={styles.qr} src={qr_image} alt="QR Code that you can scan with your mobile to access example.com" />
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+      <div className={styles.attribution}>
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="https://www.frontendmentor.io/profile/purummso">purummso</a>.
+      </div>
+    </>
   )
 }
 
